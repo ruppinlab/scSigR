87f634f88422802bb462ee5cefbbe2eb384199e6
sp00nman <fiorella.schischlik@gmail.com>
2021-03-08 16:34:32 -0500

    first working function for gep

--- R/aggregateGEP.R
+++ R/aggregateGEP.R
@@ -8,21 +8,33 @@
 #' GEP into TPM. This process was repeated in order to generate five aggregated 
 #' transcriptome replicates per cell type assume availabe cell types are > 3
 #'
-#' @param matrix 
-#' @param meta 
-#' @param cell_types 
-#' @param scale_factor 
-#' @param seed 
+#' @param scgep A matrix of normalized gene expression values, rows are genes,
+#' columns are cells
+#' @param meta A data.frame with meta data information for each cell. A mandatory
+#' column name 'Cell' matches with the column names of scgep
+#' @param meta_column Name of the column that contains the cell type information
+#' @param cell_types A vector indicating the cell types to use for generating
+#' the signature
+#' @param scale_factor Scaling factor used for normalization
+#' @param seed A number
 #'
 #' @return A matrix of aggregated GEP profiles
 #' @export
 #'
 #' @examples
+#' aggregateGEP(
+#'  livnat_mat, 
+#'  livnat_meta, 
+#'  cell_types=c("T-cell", "Malignant"),
+#'  meta_column="Cell_type",
+#'  scale_factor=10000,
+#'  seed=1,
+#'  num_gep=5)
 aggregateGEP <- function(
-  matrix,
-  meta,
-  meta_column="Celltype (major-lineage)",
+  scgep,
+  meta_data,
   cell_types,
+  meta_column="Celltype (major-lineage)",
   scale_factor=10000,
   seed = 1,
   num_gep = 5,
@@ -30,20 +42,29 @@ aggregateGEP <- function(
   
   # TODO:
   # add quality checks for input
+  if(is.data.frame(scgep)) {scgep <- as.matrix(scgep)}
+  if(verbose){
+    print("Aggregate GEP: ")
+    print(str(scgep))
+    print(scgep[1:5, 1:5])
+    print(paste("Cell type column: ", meta_column, sep=""))
+    print(paste("Seed: ", seed, sep=""))
+    print(head(meta_data))
+  }
   
-  # set seed
+  # Set seed
   set.seed(seed)
   
   # Aggregated cell types
-  agg_ct <- matrix(nrow=dim(matrix)[1], ncol=length(cell_types) * 5)
+  agg_ct <- matrix(nrow=dim(scgep)[1], ncol=length(cell_types) * 5)
   matCol <- 1
   
   for (i in 1:length(cell_types)){
     
     if(verbose) {print(cell_types[i])}
     
-    # Get cell type
-    cell_type_specific <- meta[meta[[meta_column]]==cell_types[i], ]
+    # Get cells
+    cell_type_specific <- meta_data[meta_data[[meta_column]]==cell_types[i], ]
     
     for(j in 1:num_gep){
       
@@ -55,7 +76,7 @@ aggregateGEP <- function(
       )
       
       # Aggregate the profiles by summation in non-log linear space
-      cell_gep <- Matrix::rowSums(matrix[, sampled_cells])
+      cell_gep <- Matrix::rowSums(scgep[, sampled_cells])
       
       # Write result to new matrix
       agg_ct[, matCol] <- cell_gep
@@ -65,7 +86,7 @@ aggregateGEP <- function(
   
   # Add meaningful column and row names
   colnames(agg_ct) <- paste(sort(rep(cell_types, num_gep)), seq(1:num_gep), sep=".")
-  row.names(agg_ct) <- row.names(matrix)
+  row.names(agg_ct) <- row.names(scgep)
   
   # ..and normalized each population-level GEP to scale factor 
   agg_ct_Colsum <- t(scale_factor * (t(agg_ct) / Matrix::colSums(agg_ct)))
